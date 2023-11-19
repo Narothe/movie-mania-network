9a4a3f0dc562b71a9635538437138f683806057b
Narothe <kuba.dojka@gmail.com>
2023-11-19 20:12:47 +0100

    navbar 80% ready

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Navbar from "./components/navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation  } from "react-router-dom";
 
 function App() {
-  return (
-      <div className="container-fluid">
-        <Navbar />
-        <div className="container">
-          <Outlet />
+    const location = useLocation();
+    const isHomePage = location.pathname === "/home";
+
+    return (
+        <div className="container-fluid">
+            {/*navbar visible only in /home subpage*/}
+            {isHomePage && <Navbar/>}
+            <div className="container">
+                <Outlet/>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./navbar.css";
 
 const Navbar = () => {
     return (
-        <div>
-            <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                <li className="nav-item" role="presentation">
-                    <Link to="/categories" className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
-                          aria-controls="pills-home" aria-selected="true">Categories</Link>
-                </li>
-                <li className="nav-item" role="presentation">
-                    <Link to="/statistics" className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
-                          aria-controls="pills-home" aria-selected="true">Statistics</Link>
-                </li>
-                <li className="nav-item" role="presentation">
-                    <Link to="/add_movie" className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
-                          aria-controls="pills-home" aria-selected="true">Add movie</Link>
-                </li>
-            </ul>
+        <div class="position-absolute top-50 start-50 translate-middle border border-3 rounded-2">
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-color btn-size" type="submit"> >>> </button>
+                    </form>
+                </div>
+            </nav>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/categories">Categories</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link" href="/statistics">Statistics</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         </div>
     );
 };
