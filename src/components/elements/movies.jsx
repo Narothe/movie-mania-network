8ba3c1263a491355dfb87ce9761d1347de6d573d
Narothe <kuba.dojka@gmail.com>
2023-11-21 11:01:20 +0100

    etap3 done

--- src/components/elements/movies.jsx
+++ src/components/elements/movies.jsx
@@ -1,23 +1,27 @@
 import React, {useState} from "react";
 import thumbnailImage1 from "../assets/movie_window_view/climate.jpg";
 import thumbnailImage2 from "../assets/movie_window_view/clouds.jpg";
-import thumbnailImage3 from "../assets/movie_window_view/earth.jpg";
+import thumbnailImage3 from "../assets/movie_window_view/hobbit.jpg";
 import thumbnailImage4 from "../assets/movie_window_view/fireworks.jpg";
 import thumbnailImage5 from "../assets/movie_window_view/flower_pot.jpg";
 import thumbnailImage6 from "../assets/movie_window_view/flowers.jpg";
 import thumbnailImage7 from "../assets/movie_window_view/galaxy.jpg";
 import thumbnailImage8 from "../assets/movie_window_view/ghost.jpg";
 
+import goodRate from "../assets/symbols/goodRate.png"
+import midRate from "../assets/symbols/midRate.png"
+import badRate from "../assets/symbols/badRate.png"
+
 const Movies = () => {
     const images = [
-        { src: thumbnailImage1, title: "Climate" },
-        { src: thumbnailImage2, title: "Clouds" },
-        { src: thumbnailImage3, title: "Earth" },
-        { src: thumbnailImage4, title: "Fireworks" },
-        { src: thumbnailImage5, title: "Flower Pot" },
-        { src: thumbnailImage6, title: "Flowers" },
-        { src: thumbnailImage7, title: "Galaxy" },
-        { src: thumbnailImage8, title: "Ghost" },
+        { src: thumbnailImage1, title: "Climate", type: "Documentary", description:"Why climate change is bad.", rate: 9 },
+        { src: thumbnailImage2, title: "Clouds", type: "Documentary", description:"How about cloud formation are forming.", rate: 8 },
+        { src: thumbnailImage3, title: "Hobbit", type: "Adventure", description:"Amazing adventure of little Hobbit", rate: 9 },
+        { src: thumbnailImage4, title: "Fireworks", type: "Documentary", description:"Why fireworks are on fire.", rate: 6 },
+        { src: thumbnailImage5, title: "Flower Pot", type: "Documentary", description:"Safe tips to keep plans in home.", rate: 4 },
+        { src: thumbnailImage6, title: "Flowers", type: "Documentary", description:"Different varieties of flowers.", rate: 2 },
+        { src: thumbnailImage7, title: "Galaxy", type: "Documentary", description:"Stars in our galaxy.", rate: 10 },
+        { src: thumbnailImage8, title: "Ghostbusters", type: "Documentary", description:"Brave team want to catch ghosts.", rate: 9 },
     ];
 
     const itemsPerPage = 5;
@@ -78,7 +82,15 @@ const Movies = () => {
                         {hoveredImage === index && (
                             <div className="thumbnail-info">
                                 <p>{image.title}</p>
-                                {/* Dodaj inne informacje, które chcesz wyświetlić */}
+                                <p>{image.description}</p>
+                                <p>Rate: {image.rate}/10</p>
+                                {image.rate >= 8 ? (
+                                    <img className="rate-image" src={goodRate} alt="Good Rate" />
+                                ) : image.rate >= 4 ? (
+                                    <img className="rate-image" src={midRate} alt="Mid Rate" />
+                                ) : (
+                                    <img className="rate-image" src={badRate} alt="Bad Rate" />
+                                )}
                             </div>
                         )}
                     </div>
