e3c0bdd7e868cbcf193cac96ad6768196ffe0318
Narothe <kuba.dojka@gmail.com>
2023-12-10 19:52:01 +0100

    add

--- src/components/subpages/Details.jsx
+++ src/components/subpages/Details.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import moviesDetails from "../elements/MoviesData";
 
 const Details = () => {
-    const { id } = useParams();
+    const {id} = useParams();
     const selectedImage = moviesDetails.find((img) => img.id.toString() === id);
 
     if (!selectedImage) {
@@ -11,11 +11,23 @@ const Details = () => {
     }
 
     return (
-        <div>
-            <h2>{selectedImage.id}</h2>
-            <h2>{selectedImage.title}</h2>
-            <p>{selectedImage.description}</p>
-            {/* Dodaj dowolne inne informacje, które chcesz wyświetlić */}
+        <div className="details-position">
+            <div className="details-split">
+                <div className="details-names">
+                    <h2>{selectedImage.title}</h2>
+                    <p>{selectedImage.description}</p>
+                </div>
+                <div className="details-type-rate">
+                    <h4>{selectedImage.type}</h4>
+                </div>
+            </div>
+            <div className="details-details">
+                <img className="details-image" src={selectedImage.src} alt={selectedImage.title}/>
+                <p className="details-p">{selectedImage.long_description}</p>
+                <div className="details-moreInfo">
+
+                </div>
+            </div>
         </div>
     );
 };
