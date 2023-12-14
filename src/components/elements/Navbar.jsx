818f8c662ead9e005f5cc604d629355340c301c7
Narothe <kuba.dojka@gmail.com>
2023-12-15 00:01:00 +0100

    login done

--- src/components/elements/Navbar.jsx
+++ src/components/elements/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div className="container-fluid">
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-color btn-size" type="submit"> >>></button>
+                        <button className="btn btn-color navbar-btn-size" type="submit"> >>></button>
                     </form>
                 </div>
             </nav>
--- src/components/subpages/Login.jsx
+++ src/components/subpages/Login.jsx
@@ -5,7 +5,6 @@ const Login = () => {
     const handleSubmit = () => {
         console.log('Zalogowano')
         alert('Zalogowano! (aktualnie brak możliwości zalogowania)');
-
     };
     return (
         <div className="login-container">
@@ -13,22 +12,37 @@ const Login = () => {
             <div className="login-center">
                 <div className="login-space-container">
                     <div className="login-margin">
-                        <div className="login-elements">
-                            <form onSubmit={handleSubmit} className="login-form">
-                                <label>
+                        <form onSubmit={handleSubmit} className="login-form">
+                            <div className="add-labels">
+                                <label className="login-elements">
                                     Login:
                                     <input
-                                        className="login-form-control me-2"
+                                        className="login-form-control me-2 add-margin"
                                         type="text"
                                         name="login"
                                     />
                                 </label>
-
-                            </form>
-
-                            {/*<p>Login:</p>*/}
-                        </div>
+                            </div>
+                            <label className="login-elements">
+                                Password:
+                                <input
+                                    className="login-form-control me-2 add-margin"
+                                    type="password"
+                                    name="password"
+                                />
+                            </label>
+                            <br/>
+                            <br/>
+                            <button className="btn invert-btn-color" type="submit">Login</button>
+                        </form>
                     </div>
+                </div>
+            </div>
+            <div className="login-margin-top ">
+                <p className="login-center login-bigger-font">Or:</p>
+
+                <div className="login-center">
+                    <button className="btn btn-color btn-size" type="submit">Register</button>
                 </div>
             </div>
         </div>
