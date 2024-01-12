913585eac73d95c9109e8e9cf1070a204dbaccea
Narothe <kuba.dojka@gmail.com>
2024-01-12 15:32:16 +0100

    phase_5
    feat: category titles, images

--- src/components/pages/categories/Category.jsx
+++ src/components/pages/categories/Category.jsx
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from "react";
-import {Link, useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import Footnote from "../../elements/footnote/Footnote";
 import TopContainer from "../../elements/topContainer/TopContainer";
 import HorizontalGap from "../../elements/topContainer/HorizontalGap";
-// import Movies from "../../elements/Movies";
 import styles from "./Category.module.css";
-import { useSpring, animated } from 'react-spring';
-import {useAuth} from "../../utils/AuthContext";
+import { useSpring, animated } from "react-spring";
+import { useAuth } from "../../utils/AuthContext";
 import LoggedUser from "../../elements/loggedUser/LoggedUser";
 import SignInButton from "../../elements/signinButton/SignInButton";
 import axios from "axios";
@@ -16,16 +15,18 @@ const Category = () => {
     const props = useSpring({opacity: 1, from: {opacity: 0}});
     const [movies, setMovies] = useState([]);
     const {token} = useAuth();
-
     const {categoryName} = useParams();
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-        document.title = 'Movie Mania Network';
+        document.title = "Movie Mania Network";
 
         const fetchData = async () => {
             try {
                 const response = await axios.get(`https://at.usermd.net/api/movies`);
-                const filteredMovies = response.data.filter(movie => movie.genre === categoryName);
+                const filteredMovies = response.data.filter(
+                    (movie) => movie.genre === categoryName
+                );
                 setMovies(filteredMovies);
             } catch (error) {
                 console.error(error);
@@ -35,25 +36,43 @@ const Category = () => {
         fetchData();
     }, []);
 
-
     return (
         <animated.div style={props}>
             {token ? <LoggedUser/> : <SignInButton/>}
             <div className={`container ${styles.categoryContainer}`}>
                 <div className={styles.properWidth}>
-                    <TopContainer text={'Category'}/>
+                    <TopContainer text={"Category"}/>
                 </div>
                 <HorizontalGap gap={`Category: ${categoryName}`}/>
 
-                {movies.map((movie) => (
-                    <Link className={styles.textPosition} key={movie.id} to={`/details/${movie.id}`}>
-                        <p className={styles.textLook}>{movie.title}</p>
-                    </Link>
-                ))}
+                <div className={styles.twoContainers}>
+                    <div className={styles.firstContainer}>
+                        {movies.map((movie) => (
+                            <div key={movie.id}>
+                                <Link
+                                    to={`/details/${movie.id}`}
+                                    className={styles.textLook}
+                                    onMouseEnter={() => setSelectedMovie(movie)}
+                                    onMouseLeave={() => setSelectedMovie(null)}
+                                >{movie.title}</Link>
+                            </div>
+                        ))}
+                    </div>
+                    {/*obrazek*/}
+                    <div className={styles.secondContainer}>
+                        {selectedMovie && (
+                            <img
+                                className={styles.movieImage}
+                                src={selectedMovie.image}
+                                alt={selectedMovie.image}
+                            />
+                        )}
+                    </div>
+                </div>
                 <Footnote/>
             </div>
         </animated.div>
     );
-}
+};
 
 export default Category;
